7145d1bf1ebd1238c68d26d51bee55b795faab78
Inchino <alessandro.incalza22@gmail.com>
2025-01-24 11:35:49 +0100

    Novità

--- apple-music/src/components/Novità.tsx
+++ apple-music/src/components/Novità.tsx
@@ -1,13 +1,40 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const Novità = () => {
   return (
     <>
-    <h1>Novità</h1>
-    <hr/>
-    <Container>
-
-    </Container>
+      <h1>Novità</h1>
+      <hr />
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  <h6>NUOVA STAZIONE RADIO</h6>
+                </Card.Title>
+                <Card.Text>
+                  Rilassati, al resto pensiamo noi. Ascolta Apple Music Chill
+                </Card.Text>
+              </Card.Body>
+              <Card.Img variant="bottom" src="..\assets\images\1a.png" />
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  <h6>NUOVA STAZIONE RADIO</h6>
+                </Card.Title>
+                <Card.Text>
+                  Ecco la nuova casa della musica latina
+                </Card.Text>
+              </Card.Body>
+              <Card.Img variant="bottom" src="..\assets\images\1b.png" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
